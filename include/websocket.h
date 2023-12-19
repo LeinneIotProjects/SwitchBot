75e6f0796c13553e44c61d714e3837dcf06185a9
LeinneSW <nno88551@naver.com>
2023-12-20 00:19:43 +0900

    웹소켓 예외처리 개선

--- include/websocket.h
+++ include/websocket.h
@@ -10,40 +10,7 @@
 #include "storage.h"
 #include "battery.h"
 
-#define DEFAULT_WEBSOCKET_URL "wss://iot.leinne.net/ws"
-
-const char* pem = R"rawliteral(
------BEGIN CERTIFICATE-----
-MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMjAwOTA0MDAwMDAw
-WhcNMjUwOTE1MTYwMDAwWjAyMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg
-RW5jcnlwdDELMAkGA1UEAxMCUjMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
-AoIBAQC7AhUozPaglNMPEuyNVZLD+ILxmaZ6QoinXSaqtSu5xUyxr45r+XXIo9cP
-R5QUVTVXjJ6oojkZ9YI8QqlObvU7wy7bjcCwXPNZOOftz2nwWgsbvsCUJCWH+jdx
-sxPnHKzhm+/b5DtFUkWWqcFTzjTIUu61ru2P3mBw4qVUq7ZtDpelQDRrK9O8Zutm
-NHz6a4uPVymZ+DAXXbpyb/uBxa3Shlg9F8fnCbvxK/eG3MHacV3URuPMrSXBiLxg
-Z3Vms/EY96Jc5lP/Ooi2R6X/ExjqmAl3P51T+c8B5fWmcBcUr2Ok/5mzk53cU6cG
-/kiFHaFpriV1uxPMUgP17VGhi9sVAgMBAAGjggEIMIIBBDAOBgNVHQ8BAf8EBAMC
-AYYwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMBIGA1UdEwEB/wQIMAYB
-Af8CAQAwHQYDVR0OBBYEFBQusxe3WFbLrlAJQOYfr52LFMLGMB8GA1UdIwQYMBaA
-FHm0WeZ7tuXkAXOACIjIGlj26ZtuMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcw
-AoYWaHR0cDovL3gxLmkubGVuY3Iub3JnLzAnBgNVHR8EIDAeMBygGqAYhhZodHRw
-Oi8veDEuYy5sZW5jci5vcmcvMCIGA1UdIAQbMBkwCAYGZ4EMAQIBMA0GCysGAQQB
-gt8TAQEBMA0GCSqGSIb3DQEBCwUAA4ICAQCFyk5HPqP3hUSFvNVneLKYY611TR6W
-PTNlclQtgaDqw+34IL9fzLdwALduO/ZelN7kIJ+m74uyA+eitRY8kc607TkC53wl
-ikfmZW4/RvTZ8M6UK+5UzhK8jCdLuMGYL6KvzXGRSgi3yLgjewQtCPkIVz6D2QQz
-CkcheAmCJ8MqyJu5zlzyZMjAvnnAT45tRAxekrsu94sQ4egdRCnbWSDtY7kh+BIm
-lJNXoB1lBMEKIq4QDUOXoRgffuDghje1WrG9ML+Hbisq/yFOGwXD9RiX8F6sw6W4
-avAuvDszue5L3sz85K+EC4Y/wFVDNvZo4TYXao6Z0f+lQKc0t8DQYzk1OXVu8rp2
-yJMC6alLbBfODALZvYH7n7do1AZls4I9d1P4jnkDrQoxB3UqQ9hVl3LEKQ73xF1O
-yK5GhDDX8oVfGKF5u+decIsH4YaTw7mP3GFxJSqv3+0lUFJoi5Lc5da149p90Ids
-hCExroL1+7mryIkXPeFM5TgO9r0rvZaBFOvV2z0gp35Z0+L4WPlbuEjN/lxPFin+
-HlUjr8gRsI3qfJOQFy/9rKIJR0Y/8Omwt/8oTWgy1mdeHmmjk7j1nYsvC9JSQ6Zv
-MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX
-nLRbwHOoq7hHwg==
------END CERTIFICATE-----
-)rawliteral";
+#define WEBSOCKET_URL "ws://leinne.net:8080/ws"
 
 typedef enum{
     CONTINUITY,
@@ -69,7 +36,7 @@ namespace ws{
             buffer[3 + i] = device[i];
         }
         esp_websocket_client_send_with_opcode(webSocket, WS_TRANSPORT_OPCODES_BINARY, buffer, device.length() + 3, portMAX_DELAY);
-        printf("[WS] Send welcome message\n");
+        cout << "[Socket] 환영 메시지를 전송했습니다.\n";
     }
 
     void sendSwitchState(ledc_channel_t channel, bool state){
@@ -86,39 +53,53 @@ namespace ws{
 
     static void eventHandler(void* object, esp_event_base_t base, int32_t eventId, void* eventData){
         esp_websocket_event_data_t* data = (esp_websocket_event_data_t*) eventData;
-        if(eventId == WEBSOCKET_EVENT_DISCONNECTED || eventId == WEBSOCKET_EVENT_ERROR){
+        if(eventId == WEBSOCKET_EVENT_DISCONNECTED){
             if(connectServer){
-                printf("[WS] Disconnected WebSocket\n");
+                cout << "[Socket] 연결이 끊어졌습니다.\n";
             }
             connectServer = false;
+        }else if(eventId == WEBSOCKET_EVENT_ERROR){
+            if(!wifi::connect){
+                return;
+            }
+
+            cout << "[Socket] esp_tls_stack_err: " << data->error_handle.esp_tls_stack_err << ", status: " << data->error_handle.esp_ws_handshake_status_code << ", socket: " << data->error_handle.esp_transport_sock_errno << "\n";
+            switch(data->error_handle.error_type){
+                case WEBSOCKET_ERROR_TYPE_NONE:
+                    cout << "[Socket] 에러 발생, type: NONE\n";
+                    break;
+                case WEBSOCKET_ERROR_TYPE_TCP_TRANSPORT:
+                    cout << "[Socket] 에러 발생, type: TCP_TRANSPORT\n";
+                    break;
+                case WEBSOCKET_ERROR_TYPE_PONG_TIMEOUT:
+                    cout << "[Socket] 에러 발생, type: PONG_TIMEOUT\n";
+                    break;
+                case WEBSOCKET_ERROR_TYPE_HANDSHAKE:
+                    cout << "[Socket] 에러 발생, type: TYPE_HANDSHAKE\n";
+                    break;
+            }
         }else if(eventId == WEBSOCKET_EVENT_DATA){
             if(data->op_code == STRING && !connectServer){
                 string device(data->data_ptr, data->data_len);
                 if(storage::getDeviceId() == device){
                     connectServer = true;
-                    printf("[WS] Connect successful.\n");
+                    std::cout << "[Socket] 서버와 연결되었습니다.\n";
                 }else{
-                    printf("[WS] FAILED. device: %s, receive: %s, len: %d\n", storage::getDeviceId().c_str(), device.c_str(), data->data_len);
+                    std::cout << "[Socket] 서버 연결 실패. 기기명 불일치 [device: " << storage::getDeviceId() << ", receive: " << device << ", len: " << data->data_len << "]\n";
                 }
             }
         }
     }
 
     void start(esp_event_handler_t handler){
-        auto url = storage::getString("websocket_url");
-        if(url.find_first_of("ws") == string::npos){
-            storage::setString("websocket_url", url = DEFAULT_WEBSOCKET_URL);
-        }
-
-        esp_websocket_client_config_t websocket_cfg = {
-            .uri = url.c_str(),
-            .cert_pem = pem,
+        esp_websocket_client_config_t socketConfig = {
+            .uri = WEBSOCKET_URL,
             .keep_alive_enable = true,
             .reconnect_timeout_ms = 1000,
         };
 
         while(webSocket == NULL){
-            webSocket = esp_websocket_client_init(&websocket_cfg);
+            webSocket = esp_websocket_client_init(&socketConfig);
         }
         esp_websocket_register_events(webSocket, WEBSOCKET_EVENT_ANY, handler, NULL);
         esp_websocket_register_events(webSocket, WEBSOCKET_EVENT_ANY, eventHandler, NULL);
